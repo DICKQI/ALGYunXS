5f4c31b02f4ac64b7ea6ed5af1e3c2f5ecfacab1
DICKQI <dickqi87@gmail.com>
2018-11-20 13:08:03 +0800

    change

--- apps/home.py
+++ apps/home.py
@@ -3,16 +3,21 @@ from django.http import JsonResponse
 from django.shortcuts import Http404
 import json
 class HomeIndex(APIView):
-    def get(self, request):
+    def get(self, requests):
         html = '欢迎来到ALGYun'
         return JsonResponse({'html':html})
-    def post(self, request):
-        params = request.body
-        try:
-            data = json.loads(params)
-            html = '欢迎' + data['postman'] + '来到ALGYun'
-            return JsonResponse({
-                'html':html
-            })
-        except:
-            return JsonResponse({'err':'输入错误'})
+    def post(self, requests):
+        # params = requests.body
+        # try:
+        #     data = json.loads(params)
+        #     html = '欢迎' + data['postman'] + '来到ALGYun'
+        #     return JsonResponse({
+        #         'html':html
+        #     })
+        # except:
+        #     return JsonResponse({'err':'输入错误'})
+        params = requests.POST.get(
+            'postman'
+        )
+        html = '欢迎' + str(params) + '来到ALGYun'
+        return JsonResponse({'html':html})
