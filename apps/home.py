cbe8f78055bc49a60a3fa639abb2b13da4148723
杉菜 <dickqi87@gmail.com>
2018-11-20 09:14:10 +0800

    change

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from django.shortcuts import Http404
 import json
 class HomeIndex(APIView):
     def get(self, requests):
@@ -7,10 +8,13 @@ class HomeIndex(APIView):
         return JsonResponse({'html':html})
     def post(self, requests):
         params = requests.body
-        data = json.loads(params)
-        html = '欢迎' + data['postman'] + '来到ALGYun'
-        password = data['password']
-        return JsonResponse({
-            'html':html,
-            'password':password
-        })
+        try:
+            data = json.loads(params)
+            html = '欢迎' + data['postman'] + '来到ALGYun'
+            password = data['password']
+            return JsonResponse({
+                'html':html,
+                'password':password
+            })
+        except:
+            return JsonResponse({'err':'输入错误'})
