972f21073b5c28e6680dd283312ad5bf65526ea0
DICKQI <dickqi87@gmail.com>
2018-11-20 13:23:59 +0800

    no message

--- apps/home.py
+++ apps/home.py
@@ -3,21 +3,16 @@ from django.http import JsonResponse
 from django.shortcuts import Http404
 import json
 class HomeIndex(APIView):
-    def get(self, requests):
+    def get(self, request):
         html = '欢迎来到ALGYun'
         return JsonResponse({'html':html})
-    def post(self, requests):
-        # params = requests.body
-        # try:
-        #     data = json.loads(params)
-        #     html = '欢迎' + data['postman'] + '来到ALGYun'
-        #     return JsonResponse({
-        #         'html':html
-        #     })
-        # except:
-        #     return JsonResponse({'err':'输入错误'})
-        params = requests.POST.get(
-            'postman'
-        )
-        html = '欢迎' + str(params) + '来到ALGYun'
-        return JsonResponse({'html':html})
+    def post(self, request):
+        params = request.body
+        try:
+            data = json.loads(params)
+            html = '欢迎' + data['postman'] + '来到ALGYun'
+            return JsonResponse({
+                'html':html
+            })
+        except:
+            return JsonResponse({'err':'输入错误'})
