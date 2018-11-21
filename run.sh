e990ace5d572533d8631289b35b71bb1ae7149ca
杉菜 <dickqi87@gmail.com>
2018-11-22 00:52:42 +0800

    no message

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uwsgi --socket 127.0.0.1:4000 --plugin python3 --buffer-size 32768 --module ALGXS.wsgi &
+uwsgi --http localhost:4000 --plugin python3 --buffer-size 32768 --module ALGXS.wsgi &
