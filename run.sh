144aeeddc06383307e9ea46409062ccb07bc56b2
杉菜 <dickqi87@gmail.com>
2018-11-22 22:44:23 +0800

    no message

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uwsgi --socket :8000 --plugin python --buffer-size 32768 --module ALGXS.wsgi &
+uwsgi --socket :8000 --buffer-size 32768 --module ALGXS.wsgi &
--- run_for_react.sh
+++ run_for_react.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uwsgi --http localhost:4000 --plugin python --buffer-size 32768 --module ALGXS.wsgi &
+uwsgi --http localhost:4000 --buffer-size 32768 --module ALGXS.wsgi &
