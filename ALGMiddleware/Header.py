d690b0d723910cfc3466f3e83c2fe97d7ce649a0
杉菜 <dickqi87@gmail.com>
2019-03-05 09:32:58 +0800

    change

--- ALGMiddleware/Header.py
+++ ALGMiddleware/Header.py
@@ -1,6 +1,6 @@
 from django.utils.deprecation import MiddlewareMixin
 
-class AllowOrigin(MiddlewareMixin):
+class ALGHeader(MiddlewareMixin):
     def process_response(self, request, response):
         response['Access-Control-Allow-Origin'] = '127.0.0.1:8080'
         return response
