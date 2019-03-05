e2727bea22c55b53167a285bb6b95776bec100e2
杉菜 <dickqi87@gmail.com>
2019-03-05 11:11:55 +0800

    转发

--- ALGMiddleware/Header.py
+++ ALGMiddleware/Header.py
@@ -2,5 +2,5 @@ from django.utils.deprecation import MiddlewareMixin
 
 class ALGHeader(MiddlewareMixin):
     def process_response(self, request, response):
-        response['Access-Control-Allow-Origin'] = '127.0.0.1:8080'
+        response['Access-Control-Allow-Origin'] = 'https://algyun.cn:83'
         return response
--- ALGXS/settings.py
+++ ALGXS/settings.py
@@ -96,7 +96,8 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
 
     'localhost:8080',
-    '127.0.0.1:8080'
+    '127.0.0.1:8080',
+    'https://algyun.cn:83'
 
 )
 CORS_ALLOW_METHODS = (
