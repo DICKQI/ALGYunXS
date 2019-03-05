27f8dadabf61d098adcec729973ba0e5ac0edf9f
杉菜 <dickqi87@gmail.com>
2019-03-05 23:42:57 +0800

    change

--- ALGXS/settings.py
+++ ALGXS/settings.py
@@ -91,13 +91,11 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
 
-    'localhost:8080',
-    '127.0.0.1:8080',
-    'https://algyun.cn:83'
+    '*'
 
 )
 CORS_ALLOW_METHODS = (
