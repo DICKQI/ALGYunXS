f77004d664fe59b331f1bb0104ef2121427d351b
杉菜 <dickqi87@gmail.com>
2019-04-03 23:50:52 +0800

    fix 自动执行脚本的bug

--- AutoExecuteScript/AutoOrderExecution.py
+++ AutoExecuteScript/AutoOrderExecution.py
@@ -1,12 +1,14 @@
 import requests
 
-
-# def deleteUnPayOrder():
-#     requests.delete('https://algyun.cn:81/autoAPI/commodity/order/')
-
+'''
+这是一个自动执行脚本
+用来自动确认收货
+每1分钟执行一次
+通过对接口进行访问的方式
+'''
 
 def ConfirmOvertimeOrder():
-    requests.get('https://algyun.cn/autoAPI/commodity/order/')
+    requests.get('https://algyun.cn:81/autoAPI/commodity/order/')
 
 
 if __name__ == '__main__':
--- AutoExecuteScript/AutoUnlockIP.py
+++ AutoExecuteScript/AutoUnlockIP.py
@@ -1,5 +1,12 @@
 import requests
 
+'''
+这是一个自动执行脚本
+用来解锁被封的IP
+每天0点执行
+通过对接口进行访问的方式
+'''
+
 def unlockIP():
     requests.delete('https://algyun.cn:81/autoAPI/IP/')
 
