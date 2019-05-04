beceb598d92a19a8e1bb8723884381c37fbc19e8
杉菜 <dickqi87@gmail.com>
2019-05-04 09:46:15 +0800

    fix: 修复删除图片路由

--- apps/market/urls.py
+++ apps/market/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('<int:cid>/order/<int:ocid>/confirm/<str:type>/', ConfirmOrderView.as_view(), name='confirmOrder'),
     # c image
     path('<int:cid>/image/', CImgView.as_view(), name='update_img'),
-    path('<int:cid>/image/<int:mid>', CImgView.as_view(), name='delete_img'),
+    path('<int:cid>/image/<int:mid>/', CImgView.as_view(), name='delete_img'),
     # classification
     path('classification/new/', CommodityClassificationView.as_view(), name='Classification'),
     path('classification/list/', CommodityClassificationView.as_view(), name='classification_list'),
--- apps/market/views/image/CImageInfo.py
+++ apps/market/views/image/CImageInfo.py
@@ -1,10 +1,12 @@
 from apps.market.models import CommodityImage, Commodity
 from rest_framework.views import APIView
+from ALGCommon.userAuthCommon import check_login
 from django.http import JsonResponse
 import oss2
 
 
 class CImgView(APIView):
+    @check_login
     def post(self, request, cid):
         '''
         更新商品图片
@@ -12,34 +14,28 @@ class CImgView(APIView):
         :param cid:
         :return:
         '''
-        if request.session.get('login') != None:
-
-            try:
-                commodity = Commodity.objects.get(id=cid)
-                if commodity.commodity_img.count() > 5:
-                    return JsonResponse({
-                        'status': False,
-                        'err': '已超出图片上限(最多5张照片)'
-                    }, status=401)
-                get_img = request.FILES.get('img')
-                img = CommodityImage.objects.create(img=get_img)
-                commodity.commodity_img.add(img)
-                commodity.save()
-                return JsonResponse({
-                    'status': True,
-                    'result': img.id
-                })
-            except:
+        try:
+            commodity = Commodity.objects.get(id=cid)
+            if commodity.commodity_img.count() > 5:
                 return JsonResponse({
                     'status': False,
-                    'err': '意料以外的错误'
-                }, status=403)
-        else:
+                    'err': '已超出图片上限(最多5张照片)'
+                }, status=401)
+            get_img = request.FILES.get('img')
+            img = CommodityImage.objects.create(img=get_img)
+            commodity.commodity_img.add(img)
+            commodity.save()
+            return JsonResponse({
+                'status': True,
+                'result': img.id
+            })
+        except:
             return JsonResponse({
                 'status': False,
-                'err': '你还未登录'
-            }, status=401)
+                'err': '意料以外的错误'
+            }, status=403)
 
+    @check_login
     def delete(self, request, cid, mid):
         '''
         给文章移除图片
@@ -48,46 +44,33 @@ class CImgView(APIView):
         :param mid:
         :return:
         '''
-        if request.session.get('login'):
-            commodity = Commodity.objects.filter(id=cid)
-            if not commodity.exists():
-                return JsonResponse({
-                    'status': False,
-                    'err': '商品不存在'
-                }, status=403)
-            img = CommodityImage.objects.filter(id=mid)
-            if not img.exists():
-                return JsonResponse({
-                    'status': False,
-                    'err': '图片不存在'
-                }, status=403)
-            try:
-                commodity.commodity_img.remove(img)
-            except:
-                return JsonResponse({
-                    'status': False,
-                    'err': '删除失败'
-                }, status=403)
-            commodity.save()
-            try:
-                '''删除oss文件'''
-                object_name = 'media/' + str(img)
-                auth = oss2.Auth('LTAIpK0JtS9hsWkG', 'cQpsrRs3Nhv6hTRpEMuUA2pjX6BlWs')
-                bucket = oss2.Bucket(auth, 'oss-cn-shenzhen.aliyuncs.com', 'algyunxs')
-                bucket.delete_object(object_name)
-                '''同时删除数据库表的内容'''
-                img.delete()
-            except:
-                return JsonResponse({
-                    'status': False,
-                    'err': '删除失败'
-                }, status=403)
-            return JsonResponse({
-                'status': True,
-                'result': '删除成功'
-            })
-        else:
+        commodity = Commodity.objects.filter(id=cid)
+        if not commodity.exists():
             return JsonResponse({
                 'status': False,
-                'err': '你还没登录呢'
-            }, status=401)
+                'err': '商品不存在'
+            }, status=403)
+        img = CommodityImage.objects.filter(id=mid)
+        if not img.exists():
+            return JsonResponse({
+                'status': False,
+                'err': '图片不存在'
+            }, status=403)
+        try:
+            commodity.commodity_img.remove(img)
+            '''删除oss文件'''
+            object_name = 'media/' + str(img)
+            auth = oss2.Auth('LTAIpK0JtS9hsWkG', 'cQpsrRs3Nhv6hTRpEMuUA2pjX6BlWs')
+            bucket = oss2.Bucket(auth, 'oss-cn-shenzhen.aliyuncs.com', 'algyunxs')
+            bucket.delete_object(object_name)
+            '''同时删除数据库表的内容'''
+            img.delete()
+        except:
+            return JsonResponse({
+                'status': False,
+                'err': '删除失败'
+            }, status=403)
+        return JsonResponse({
+            'status': True,
+            'result': '删除成功'
+        })
